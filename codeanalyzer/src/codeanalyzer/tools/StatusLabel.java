0ad731d38d0ab84c681a7c0eba746c378e2714c8
marat <marat@pc>
2014-02-19 21:37:36 +0300

    1

--- codeanalyzer/src/codeanalyzer/tools/StatusLabel.java
+++ codeanalyzer/src/codeanalyzer/tools/StatusLabel.java
@@ -19,7 +19,7 @@ public class StatusLabel {
 	    
 	    Label lblNewLabel = new Label(comp, SWT.BORDER);
 //	    lblNewLabel.setBounds(0, 0, 224, 298);
-	    lblNewLabel.setText("home");
+	    lblNewLabel.setText("home1");
 
 	  }
 	} 

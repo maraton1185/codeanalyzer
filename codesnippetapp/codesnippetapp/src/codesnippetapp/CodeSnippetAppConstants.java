43605734bda62af628d3b5cd5269142d1c133c95
Enikeev <Enikeev@Marat>
2014-02-20 13:59:11 +0400

    delete

--- codesnippetapp/codesnippetapp/src/codesnippetapp/CodeSnippetAppConstants.java
+++
@@ -1,8 +0,0 @@
-package codesnippetapp;
-
-public class CodeSnippetAppConstants {
-	public static final String NEW_SNIPPET_EVENT = "onAddNewSnippet";
-	public static final String DELETE_SNIPPET_EVENT = "onDeleteSnippet";
-	public static final String SELECT_SNIPPET_EVENT = "onSelectSnippet";
-	public static final String SAVE_SNIPPET_EVENT = "onSaveSnippet";
-}

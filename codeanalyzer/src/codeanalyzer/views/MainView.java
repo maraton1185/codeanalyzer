4f6c6eea3c55fc2d5c56eb4e7e602f4baf274870
marat <marat@pc>
2014-02-23 21:00:43 +0300

    OPEN - task 5: статус активации https://github.com/maraton1185/codeanalyzer/issues/issue/5

--- codeanalyzer/src/codeanalyzer/views/MainView.java
+++ codeanalyzer/src/codeanalyzer/views/MainView.java
@@ -10,6 +10,14 @@ import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.MUILabel;
+import org.eclipse.e4.ui.model.application.ui.basic.MTrimBar;
+import org.eclipse.e4.ui.model.application.ui.basic.MTrimElement;
+import org.eclipse.e4.ui.model.application.ui.menu.MHandledToolItem;
+import org.eclipse.e4.ui.model.application.ui.menu.MMenuFactory;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolBar;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolBarElement;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolControl;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolItem;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.swt.modeling.EMenuService;
 import org.eclipse.jface.viewers.TableViewer;
@@ -34,10 +42,10 @@ public class MainView {
 	public void postConstruct(Composite parent, EMenuService menuService) {
 	
 		//
-//		showStatus();
+		showStatus();
 		
-		MUILabel element = (MUILabel)modelService.find(Strings.get("model_id_activate"), application);
-		element.setLabel(pico.get(IAuthorize.class).getInfo().ShortMessage());
+//		MUILabel element = (MUILabel)modelService.find(Strings.get("model_id_activate"), application);
+//		element.setLabel(pico.get(IAuthorize.class).getInfo().ShortMessage());
 		
 		objectsTree = new TableViewer(parent);
 		
@@ -50,8 +58,35 @@ public class MainView {
 
 	@Inject @Optional
 	public void  getEvent(@UIEventTopic(UPDATE) String status) {
-		MUILabel element = (MUILabel)modelService.find(Strings.get("model_id_activate"), application);
-		element.setLabel(status);
+		
+		MHandledToolItem toolItem = MMenuFactory.INSTANCE.createHandledToolItem();
+
+//		toolItem.setIconURI("platform:/plugin/com.toedter.e4.ui.workbench.addons/icons/fastview_restore.gif");
+
+//		toolItem.setCommand(command);
+		toolItem.setLabel(status);
+
+		
+		MToolBar toolbar =  (MToolBar)modelService.find("codeanalyzer.toopbar", application);
+		toolbar.getChildren().add((MToolBarElement)toolItem);
+		
+//		MUILabel element = (MUILabel)modelService.find("newid", application);
+//		element.setLabel(status);
+		//		mItem.se
+//	    mPart.setContributionURI("bundleclass://com.example.e4.rcp.todo/"
+//	        + "com.example.e4.rcp.todo.parts.DynamicPart");
+//	    partService.showPart(mPart, PartState.ACTIVATE);
+//		
+//		MUILabel element = (MUILabel)modelService.find(Strings.get("model_id_activate"), application);
+//		element.setLabel(status);
+//		element.get
+//		element.setLabel(status);
+		
+//		Composite comp = (Composite)toolbar.getWidget();
+//		comp.redraw();
+//		comp.update();
+//		comp.layout();
+		//		toolbar.notifyAll();
 	}
 	
 	private void showStatus() {

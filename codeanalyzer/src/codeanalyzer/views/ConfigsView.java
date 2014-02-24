4781f71e8a0e1ed1ab87b9bd716da551561fa4d4
marat <marat@pc>
2014-02-24 20:40:03 +0300

    OPEN - task 6: список конфигураций https://github.com/maraton1185/codeanalyzer/issues/issue/6

--- codeanalyzer/src/codeanalyzer/views/ConfigsView.java
+++ codeanalyzer/src/codeanalyzer/views/ConfigsView.java
@@ -2,8 +2,6 @@
 package codeanalyzer.views;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -14,12 +12,13 @@ import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.UIEventTopic;
-import org.eclipse.e4.ui.model.application.ui.menu.MHandledToolItem;
+import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ColumnPixelData;
+import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
@@ -31,11 +30,9 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
 import codeanalyzer.core.pico;
-import codeanalyzer.core.interfaces.IAuthorize;
 import codeanalyzer.core.interfaces.IDb;
 import codeanalyzer.core.interfaces.IDbManager;
 import codeanalyzer.utils.Const;
-import codeanalyzer.utils.Strings;
 
 public class ConfigsView {
 	
@@ -44,7 +41,8 @@ public class ConfigsView {
 	
 	private final Image ACTIVE = getImage("active.png");
 	private final Image NONACTIVE = getImage("nonactive.png");
-//	private final Image UNCHECKED = getImage("unchecked.png");
+	private final Image LOADED = getImage("loaded_with_table.png");
+	private final Image NOT_LOADED = getImage("not_loaded.png");
 	
 	private TableViewer viewer;
 
@@ -66,11 +64,16 @@ public class ConfigsView {
 	@PostConstruct
 	public void postConstruct(Composite parent, final IEclipseContext ctx) {
 		
-		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
+		Composite tableComposite = new Composite(parent, SWT.NONE);
+		TableColumnLayout tableColumnLayout = new TableColumnLayout();
+		tableComposite.setLayout(tableColumnLayout);
+		
+		viewer = new TableViewer(tableComposite, SWT.MULTI | SWT.H_SCROLL
 		      | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
 
-		createColumns();
-
+		createColumns(tableColumnLayout);
+		
+		
 		// make lines and header visible
 		final Table table = viewer.getTable();
 //		table.setHeaderVisible(true);
@@ -94,12 +97,13 @@ public class ConfigsView {
 		});
 	}
 
-	private void createColumns() {
+	private void createColumns(TableColumnLayout tableColumnLayout) {
 
-		TableViewerColumn colRole = new TableViewerColumn(viewer, SWT.NONE);
-		colRole.getColumn().setWidth(30);
-		colRole.getColumn().setText("");
-		colRole.setLabelProvider(new ColumnLabelProvider() {
+		TableViewerColumn column;
+		
+		column = new TableViewerColumn(viewer, SWT.NONE);
+//		column.getColumn().setText("");
+		column.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element) {
 				return null; // no string representation, we only want to
@@ -117,10 +121,13 @@ public class ConfigsView {
 			}
 		});
 
-		TableViewerColumn colDesc = new TableViewerColumn(viewer, SWT.NONE);
-		colDesc.getColumn().setWidth(150);
-//		colDesc.getColumn().setText("Описание");
-		colDesc.setLabelProvider(new ColumnLabelProvider() {
+		tableColumnLayout.setColumnData(column.getColumn(), 
+		      new ColumnPixelData(30));
+//				new ColumnWeightData(20, 30, true)); 
+		
+		
+		column = new TableViewerColumn(viewer, SWT.NONE);
+		column.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element) {
 				// Person p =
@@ -128,6 +135,35 @@ public class ConfigsView {
 			}
 		});
 
+		tableColumnLayout.setColumnData(column.getColumn(), 
+					new ColumnWeightData(50, 150, true));
+		
+		column = new TableViewerColumn(viewer, SWT.NONE);
+		column.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				return null; // no string representation, we only want to
+								// display the image
+			}
+			@Override
+			public Image getImage(Object element) {
+				IDb info = (IDb) element;
+				switch (info.getState()) {
+				case Loaded:
+					return LOADED;
+				case notLoaded:
+					return NOT_LOADED;
+//				case LoadedWithLinkTable:
+//					return NOT_LOADED;
+				default:
+					return NOT_LOADED;
+				}				
+			}
+		});
+
+		tableColumnLayout.setColumnData(column.getColumn(), 
+		      new ColumnPixelData(30));
+//				new ColumnWeightData(20, 30, true));
 
 	}
 	

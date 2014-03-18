57fa804a368a06a6036e9c3a410ace9dfcb57ad9
Enikeev <Enikeev@Marat>
2014-03-18 15:32:50 +0400

    OPEN - task 8: создание книг https://github.com/maraton1185/codeanalyzer/issues/issue/8

--- codeanalyzer/src/codeanalyzer/core/AppManager.java
+++ codeanalyzer/src/codeanalyzer/core/AppManager.java
@@ -171,6 +171,7 @@ public class AppManager {
 
 			BookInfo book = window.getContext().get(BookInfo.class);
 			book.closeConnection();
+			AppManager.ss.setSelection(null);
 			return true;
 		}
 	}

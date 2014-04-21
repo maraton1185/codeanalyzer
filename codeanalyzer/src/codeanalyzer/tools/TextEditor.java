fd60ebe7fb59f66dbd1cb09c9ad52e3ce4141eec
Enikeev <Enikeev@Marat>
2014-04-21 16:58:25 +0400

    OPEN - task 9: заполнение секции книги https://github.com/maraton1185/codeanalyzer/issues/issue/9

--- codeanalyzer/src/codeanalyzer/tools/TextEditor.java
+++ codeanalyzer/src/codeanalyzer/tools/TextEditor.java
@@ -9,10 +9,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.ProgressEvent;
 import org.eclipse.swt.browser.ProgressListener;
-import org.eclipse.swt.browser.StatusTextEvent;
-import org.eclipse.swt.browser.StatusTextListener;
-import org.eclipse.swt.events.ControlAdapter;
-import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -27,37 +23,14 @@ public class TextEditor extends Composite {
 
 	public TextEditor(Composite parent, int style) {
 		super(parent, style);
-		setLayout(new GridLayout(1, true));
+		setLayout(new GridLayout(2, false));
 
 		browser = new Browser(this, SWT.NONE);
 		browser.setJavascriptEnabled(true);
-		browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
-		// Listen to control resized
-		browser.addControlListener(new ControlAdapter() {
-			@Override
-			public void controlResized(ControlEvent e) {
-				if (loadCompleted) {
-					browser.execute("tinyMCE.activeEditor.getContentAreaContainer().height="
-							+ (browser.getClientArea().height - 70));
-
-					super.controlResized(e);
-				}
-			}
-		});
-
-		// Set url pointed to editor
-		try {
-			Bundle bundle = FrameworkUtil.getBundle(TextEditor.class);
-			URL url_bundle = FileLocator.find(bundle, new Path(
-					"web_editor/tinymce/index.html"), null);
-			URL url_file = FileLocator.toFileURL(url_bundle);
-
-			browser.setUrl(url_file.toString());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		GridData gd;
+		gd = new GridData(GridData.FILL_BOTH);
+		browser.setLayoutData(gd);
 
 		// Set content of editor after load completed
 		browser.addProgressListener(new ProgressListener() {
@@ -72,14 +45,65 @@ public class TextEditor extends Composite {
 			}
 		});
 
+		// Listen to control resized
+		// browser.addControlListener(new ControlAdapter() {
+		// @Override
+		// public void controlResized(ControlEvent e) {
+		// if (loadCompleted) {
+		// browser.execute("tinyMCE.activeEditor.getContentAreaContainer().height="
+		// + (browser.getClientArea().height - 70));
+		//
+		// super.controlResized(e);
+		// }
+		// }
+		// });
+
+		// Set url pointed to editor
+		try {
+			Bundle bundle = FrameworkUtil.getBundle(TextEditor.class);
+			URL url_bundle = FileLocator.find(bundle, new Path(
+					"web_editor/index.html"), null);
+			URL url_file = FileLocator.toFileURL(url_bundle);
+
+			browser.setUrl(url_file.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		// Listen to status change event
-		browser.addStatusTextListener(new StatusTextListener() {
-			@Override
-			public void changed(StatusTextEvent event) {
-				browser.setData("leet-content", event.text);
-			}
-		});
+		// browser.addStatusTextListener(new StatusTextListener() {
+		// @Override
+		// public void changed(StatusTextEvent event) {
+		// browser.setData("leet-content", event.text);
+		// }
+		// });
+
+		// browser.addMouseListener(new MouseListener() {
+		//
+		// @Override
+		// public void mouseUp(MouseEvent e) {
+		// // setFocus();
+		// }
+		//
+		// @Override
+		// public void mouseDown(MouseEvent e) {
+		// setFocus();
+		// }
+		//
+		// @Override
+		// public void mouseDoubleClick(MouseEvent e) {
+		// setFocus();
+		// }
+		// });
 	}
+
+	// @Override
+	// public boolean setFocus() {
+	// browser.forceFocus();
+	// browser.execute("setFocus()");
+	// return browser.setFocus();
+	// }
 
 	/**
 	 * Set the content of the HTML editor.
